5c5a3acf01a0e025eb71050014024c7a97900836
rmyers1488 <rmyers1488@gmail.com>
2025-10-02 15:37:03 -0500

    Delete project1/javaScript directory

--- project1/javaScript/itemScript.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-$(function () {
-    console.log('item script');
-});
-$(".logo").hover(function () {
-    $(this).animate({ opacity: '0.5' }, 'slow');
-}, function () {
-    $(this).animate({ opacity: '2.0' }, 'slow');
-});
