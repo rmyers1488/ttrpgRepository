b1020a9ee06ef6834474d706a4fbd1659c626d27
rmyers1488 <rmyers1488@gmail.com>
2025-10-02 15:37:16 -0500

    Delete project1 directory

--- project1/pathfinder/characters/pathfinderCharacterArchiveScript.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    alert('archive');
-});
-$('.logo').hover(function () {
-    $(this).animate({ opacity: '0.5' }, 'slow');
-}, function () {
-    $(this).animate({ opacity: '2' }, 'slow');
-
-});
