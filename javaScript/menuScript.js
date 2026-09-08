292b7beaad1e1f6c878af3d05bb13124264cd54b
rmyers1488 <rmyers1488@gmail.com>
2026-09-07 23:24:58 -0500

    Refactor menu script and restore fetch functionality

--- javaScript/menuScript.js
+++ javaScript/menuScript.js
@@ -1,7 +1,7 @@
 fetch('./menu.json')
 .then((response) => response.json())
 .then((json) => console.log(json));
-import data from 'menu.json';
+import data from './menu.json';
 console.log(data);
 // async function populate(url){
 //     const jsonList = url;
