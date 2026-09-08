3d3888f7d0327a2fec888a34385a982bbf3a1ded
rmyers1488 <rmyers1488@gmail.com>
2026-09-08 01:00:19 -0500

    Refactor menuScript.js for improved readability

--- javaScript/menuScript.js
+++ javaScript/menuScript.js
@@ -21,7 +21,7 @@ function createMenu(obj){
     const divider = document.querySelector(".sideBar");
     const topLevel = document.createElement("div");
     console.log(obj);
-    //let element = JSON.parse(obj);
+    let element = JSON.parse(obj);
     //console.log(element[0]);
 }
 populate();
