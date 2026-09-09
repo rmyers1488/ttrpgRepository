9eaeca20f58f9f76bf7809a7d3d243518e7c787a
rmyers1488 <rmyers1488@gmail.com>
2026-09-08 23:51:06 -0500

    Refactor menu script for clarity and functionality

--- javaScript/menuScript.js
+++ javaScript/menuScript.js
@@ -21,7 +21,7 @@ function createMenu(obj){
     const divider = document.querySelector(".sideBar");
     const topLevel = document.createElement("div");
     //console.log(obj);
-    let element = JSON.parse(obj);
+    const element = JSON.parse(obj);
     console.log('element',element);
     //console.log(element[0]);
 }
