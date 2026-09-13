15a84c14532b3cfb0bb77fa8bbdd77ba1ba62c0f
rmyers1488 <rmyers1488@gmail.com>
2026-09-12 23:17:26 -0500

    Refactor menuScript.js for improved readability

--- javaScript/menuScript.js
+++ javaScript/menuScript.js
@@ -27,8 +27,8 @@ function createMenu(obj) {
     // console.log('array', array);
     console.log(obj.length);
     for (let key in obj) {
-        console.log(key);
-        // console.log(JSON.parse(key));
+        //console.log(key);
+        console.log(JSON.parse(key));
     }
 }
 populate();
