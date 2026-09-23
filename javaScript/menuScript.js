6977afdea956845a71c9b23425d63ecd597199a4
rmyers1488 <rmyers1488@gmail.com>
2026-09-23 16:27:27 -0500

    Refactor menu script for improved functionality

--- javaScript/menuScript.js
+++ javaScript/menuScript.js
@@ -47,7 +47,7 @@ function createMenu(obj) {
         // line.text ? lineElement.appendChild(line.text) : "";
         if (line.a) {
             const lineA = `<a href="${line.href}">${line.a}</a>`;
-            lineElement.appendChild(lineA);
+            // lineElement.innerHTML = lineA;
         }
         if (line.title){
             lineElement.setAttribute("title", line.title);
